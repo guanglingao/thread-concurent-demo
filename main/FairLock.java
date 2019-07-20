7a079ef2671ff670f2bf8d5ad5a6d99f5a2f667a
glinsoft <guanglingao@yeah.net>
2019-07-20 16:47:32 +0800

    FairLock

---
+++ main/FairLock.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairLock implements Runnable {
+
+    public static ReentrantLock fairLock = new ReentrantLock(true);
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                fairLock.lock();
+                System.out.println(Thread.currentThread().getName()+"获得锁");
+            }finally {
+                fairLock.unlock();
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+        FairLock r1 = new FairLock();
+        Thread t1 = new Thread(r1,"Thread_1");
+        Thread t2 = new Thread(r1,"Thread_2");
+        t1.start();
+        t2.start();
+    }
+
+}
